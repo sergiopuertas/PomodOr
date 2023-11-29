27df8b5d46330e82675681ff83766561f3376c45
ALFONSOCD <alfonso.cifuentes.darriba@udc.es>
2023-11-29 11:31:11 +0100

    Frases probadas, todo OK

--- lib/Timer/clock_view.dart
+++ lib/Timer/clock_view.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'dart:async';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:pomodor/Timer/timer_mode.dart';
@@ -21,19 +22,36 @@ class ClockView extends StatefulWidget {
 class _ClockViewState extends State<ClockView> {
 
 
+  Future<List<String>> loadMotivationalSentences() async {
+    String fileContent = await rootBundle.loadString(
+        'assets/motivation_4_work.txt');
+    return fileContent.split('\n'); // Splits the file content into lines
+  }
+
+  //sentece list:
+  List<String> motivationalSentences = [];
+  Random random = Random(); // Create a Random object
+  int sentenceIndex = 0;
+
+
+  //time
   late DateTime currentTime; // Store the current time
   late Timer timer;
+
+  //flags
   bool isPaused = false;
   bool hasStarted = false;
-  int selectedMinutes = 0;
-  int selectedSeconds = 0;
-
-
 
 
   @override
   void initState() {
     super.initState();
+    loadMotivationalSentences().then((sentences) {
+      setState(() {
+        motivationalSentences = sentences;
+      });
+    });
+
 
     final timerMode = Provider.of<TimerMode>(context, listen: false);
     // Set currentTime to the initial Work Mode time
@@ -42,15 +60,21 @@ class _ClockViewState extends State<ClockView> {
     timer = Timer.periodic(Duration(seconds: 1), (timer) {
       if (!isPaused) {
         setState(() {
-          int totalSeconds = currentTime.hour * 3600 + currentTime.minute * 60 + currentTime.second;
+          int totalSeconds = currentTime.hour * 3600 + currentTime.minute * 60 +
+              currentTime.second;
           if (totalSeconds > 0) {
             currentTime = currentTime.subtract(Duration(seconds: 1));
           } else {
             // When timer reaches zero, switch modes and reset to the initial time of the new mode
             timerMode.switchMode();
-            currentTime = timerMode.isWorkMode ? timerMode.initialWorkTime : timerMode.initialRestTime;
+            currentTime =
+            timerMode.isWorkMode ? timerMode.initialWorkTime : timerMode
+                .initialRestTime;
+            sentenceIndex = random.nextInt(motivationalSentences.length);
             // Optionally, if you want to keep the timer paused after switching modes, set isPaused to true
             // isPaused = true;
+            // Update sentenceIndex every 10 seconds
+
           }
         });
       }
@@ -58,13 +82,13 @@ class _ClockViewState extends State<ClockView> {
   }
 
 
-
   void resetTimer() {
     final timerMode = Provider.of<TimerMode>(context, listen: false);
 
     setState(() {
       // Reset to the initial time of the current mode
-      currentTime = timerMode.isWorkMode ? timerMode.initialWorkTime : timerMode.initialRestTime;
+      currentTime = timerMode.isWorkMode ? timerMode.initialWorkTime : timerMode
+          .initialRestTime;
       hasStarted = false;
       isPaused = true; // Ensure the timer is paused
     });
@@ -76,6 +100,7 @@ class _ClockViewState extends State<ClockView> {
       isPaused = true;
     });
   }
+
   void resumeTimer() {
     setState(() {
       isPaused = false;
@@ -86,9 +111,13 @@ class _ClockViewState extends State<ClockView> {
   @override
   Widget build(BuildContext context) {
     // Calculate remaining time in minutes and seconds
-    int totalSeconds = currentTime.hour * 3600 + currentTime.minute * 60 + currentTime.second;
+    int totalSeconds = currentTime.hour * 3600 + currentTime.minute * 60 +
+        currentTime.second;
     int remainingMinutes = totalSeconds ~/ 60;
     int remainingSeconds = totalSeconds % 60;
+    //sentences:
+    // Use Provider.of to access TimerMode
+    final timerMode = Provider.of<TimerMode>(context, listen: false);
 
     return Scaffold(
       appBar: AppBar(
@@ -97,24 +126,25 @@ class _ClockViewState extends State<ClockView> {
           icon: Icon(Icons.arrow_back),
           onPressed: () => Navigator.of(context).pop(),
         ),
-        backgroundColor: Colors.grey[900], // Optional: match the appBar color with the background
+        backgroundColor: Colors.grey[900],
       ),
       body: Container(
         color: Colors.grey[900],
         child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Transform.rotate(
-                angle: -pi / 2,  // -90 degrees in radians
+                angle: -pi / 2,
                 child: CustomPaint(
                   painter: ClockPainter(currentTime: currentTime),
                   size: Size(300, 300),
                 ),
               ),
               SizedBox(height: 20),
-              // Always display remaining time
               Text(
-                '${remainingMinutes.toString().padLeft(2, '0')}:${remainingSeconds.toString().padLeft(2, '0')}',
+                '${remainingMinutes.toString().padLeft(
+                    2, '0')}:${remainingSeconds.toString().padLeft(2, '0')}',
                 style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
@@ -134,15 +164,23 @@ class _ClockViewState extends State<ClockView> {
                   }
                 },
               ),
+              // Display the sentence when the clock is in rest mode
+
+              if (!timerMode.isWorkMode && motivationalSentences.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    motivationalSentences[sentenceIndex],
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
             ],
           ),
         ),
       ),
     );
   }
-
-
-
 }
 
 
@@ -226,6 +264,5 @@ class ClockPainter extends CustomPainter {
     return true;
   }
 }
-
 
 
